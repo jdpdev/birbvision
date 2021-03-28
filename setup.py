f58b5dfc8bad801e01044b93e0bc20c83ff385b2
Jason DuPertuis <gallahad@me.com>
2021-03-28 16:53:55 -0400

    adjust

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
 setup(name='birbvision',
-      version='0.1.1',
+      version='0.1.2',
       description='Bird classifier for Birbvision',
       author='Jason DuPertuis',
       author_email='jdpdev@jdpdev.net',
       packages=['birbvision'],
+      package_data={'mypkg': ['aiy/lite-model_aiy_vision_classifier_birds_V1_3.tflite', 'aiy/*.txt']}
      )
